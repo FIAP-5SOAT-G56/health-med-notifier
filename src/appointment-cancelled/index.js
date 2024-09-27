64eeeda3a2b5dbf9d18e155cc59cf89c0f1b5ad1
Matheus Borges <matob@live.com>
2024-09-27 06:07:45 -0300

    feat: add lambda function to send mail when an appointment is cancelled

---
+++ src/appointment-cancelled/index.js
@@ -0,0 +1,46 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+async function processMessageAsync(payload) {
+  try {
+    console.log(`Processing message ${payload.body}`);
+    const message = JSON.parse(payload.body).Message;
+
+    console.log(`Message content: ${message}`);
+
+    const { id, email, doctor, pacient, date, start, end } = JSON.parse(message);
+
+    const msg = {
+      subject: "Health&Med - Consulta cancelada",
+      from: process.env.SENDGRID_EMAIL,
+      to: email,
+      templateId: process.env.SENDGRID_APPOINTMENT_CANCELLED_TEMPLATE_ID,
+      dynamicTemplateData: {
+        id,
+        doctor,
+        pacient,
+        date,
+        start,
+        end,
+      },
+    };
+
+    await sgMail
+      .send(msg)
+      .then(() => console.log(`Email sent to appointment with id: ${id}`))
+      .catch((error) => {
+        console.error(`Error sending email to appointment with id: ${id}`);
+        console.error(error)
+      })
+  } catch (err) {
+    console.error(`Error processing message ${payload?.Message}`);
+    console.error(err);
+    throw err;
+  }
+}
+
+exports.handler = async (event, context) => {
+  for (const message of event.Records) {
+    await processMessageAsync(message)
+  }
+};
